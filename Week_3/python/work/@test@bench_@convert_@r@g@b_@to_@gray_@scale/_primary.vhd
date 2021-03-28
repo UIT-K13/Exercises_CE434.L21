d3acdef1abf2089db44bd714bea1a181c58a9151
Duy Tran Van <58819775+vanduytran@users.noreply.github.com>
2021-03-28 17:33:28 +0700

    Add files via upload
    full completion code

---
+++ Week_3/python/work/@test@bench_@convert_@r@g@b_@to_@gray_@scale/_primary.vhd
@@ -0,0 +1,29 @@
+library verilog;
+use verilog.vl_types.all;
+entity TestBench_Convert_RGB_To_Gray_Scale is
+    generic(
+        IMAGE_HEIGHT    : integer := 600;
+        IMAGE_WIDTH     : integer := 450;
+        TOTAL_NUMBER_CONVERT: vl_notype;
+        DATA_COLOR_WIDTH: integer := 8;
+        DATA_IN_WIDTH   : vl_notype;
+        FIXED_POINT_WIDTH: integer := 32;
+        POINT_POSITION  : vl_notype;
+        ADRR_WIDTH_BRAM : integer := 19;
+        LINK_BRAM_READ  : string  := "..\data_pre_and_post_convert\original_image.txt";
+        LINK_BRAM_WRITE : string  := "..\data_pre_and_post_convert\converted_image.txt";
+        t               : integer := 10
+    );
+    attribute mti_svvh_generic_type : integer;
+    attribute mti_svvh_generic_type of IMAGE_HEIGHT : constant is 1;
+    attribute mti_svvh_generic_type of IMAGE_WIDTH : constant is 1;
+    attribute mti_svvh_generic_type of TOTAL_NUMBER_CONVERT : constant is 3;
+    attribute mti_svvh_generic_type of DATA_COLOR_WIDTH : constant is 1;
+    attribute mti_svvh_generic_type of DATA_IN_WIDTH : constant is 3;
+    attribute mti_svvh_generic_type of FIXED_POINT_WIDTH : constant is 1;
+    attribute mti_svvh_generic_type of POINT_POSITION : constant is 3;
+    attribute mti_svvh_generic_type of ADRR_WIDTH_BRAM : constant is 1;
+    attribute mti_svvh_generic_type of LINK_BRAM_READ : constant is 1;
+    attribute mti_svvh_generic_type of LINK_BRAM_WRITE : constant is 1;
+    attribute mti_svvh_generic_type of t : constant is 1;
+end TestBench_Convert_RGB_To_Gray_Scale;
